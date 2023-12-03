64707c4693256917e05e6f2cf1659458d21861e5
nntthuong <thuong.nn204855@gmail.com>
2023-12-03 22:19:05 +0700

    update GroupChat

--- Client/src/utils/services.js
+++ Client/src/utils/services.js
@@ -1,7 +1,7 @@
 export const baseUrl = "http://localhost:8081";
 
 export const postRequest = async (url, body) => {
-  //   console.log(url);
+  // console.log(body);
   const response = await fetch(url, {
     method: "POST",
     headers: {
@@ -9,6 +9,24 @@ export const postRequest = async (url, body) => {
     },
     body,
   });
+
+  const data = await response.json();
+  if (!response.ok) {
+    let message;
+
+    if (data?.message) {
+      message = data.message;
+    } else {
+      message = data;
+    }
+    return { error: true, message };
+  }
+
+  return data;
+};
+export const getRequest = async (url) => {
+  // console.log(body);
+  const response = await fetch(url);
 
   const data = await response.json();
   if (!response.ok) {
