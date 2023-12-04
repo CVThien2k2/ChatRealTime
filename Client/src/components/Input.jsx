a2302b4b3ce4fc209586c8edb57eb5340a32710c
nntthuong <thuong.nn204855@gmail.com>
2023-12-04 15:39:43 +0700

    design ui

--- Client/src/components/Input.jsx
+++ Client/src/components/Input.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-import { Paperclip, Image, SendFill } from "react-bootstrap-icons";
+import {
+  Paperclip,
+  Image,
+  SendFill,
+  EmojiLaughing,
+} from "react-bootstrap-icons";
 
 const Input = () => {
   return (
     <div className="Input">
-      <input type="text" placeholder="Đang nhập..." />
+      <div className="emojiAndInput">
+        <div className="emojiContainer">
+          <EmojiLaughing className="emoji" size={15} />
+        </div>
+
+        <input type="text" placeholder="Your message here..." />
+      </div>
       <div className="send">
-        <Paperclip />
-        <input type="text" style={{ display: "none" }} id="file" />
-        <label htmlFor="file">
-          <Image />
-        </label>
+        <div className="sendFile">
+          {/* <input type="text" style={{ display: "none" }} id="file" /> */}
+          <Paperclip className="file" />
+        </div>
         <button className="sendButton">
           <SendFill color="white" size={15} />
         </button>
