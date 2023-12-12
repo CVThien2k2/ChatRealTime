6489c9ffa0a6cc98a91440b02455f301581bdef0
nntthuong <thuong.nn204855@gmail.com>
2023-12-12 12:04:17 +0700

    update profile

--- Client/src/components/chat/Profile.jsx
+++ Client/src/components/chat/Profile.jsx
@@ -5,11 +5,11 @@ import { postRequest, baseUrl } from "../../utils/services";
 import { Plus } from "react-bootstrap-icons";
 
 export default function Profile() {
-  /////////////////
   const { user } = useContext(AuthContext);
   const [profileData, setProfileData] = useState(user);
   const [editMode, setEditMode] = useState(false);
   console.log(profileData);
+  // ////////////////////////
   const handleFileInputChange = (event) => {
     const file = event.target.files[0];
     const reader = new FileReader();
@@ -23,23 +23,24 @@ export default function Profile() {
       reader.readAsDataURL(file);
     }
   };
-  // const updateUserProfile = async (updatedProfileData) => {
-  //   try {
-  //     const response = await postRequest(
-  //       `${baseUrl}/auth/create`,
-  //       JSON.stringify(updatedProfileData)
-  //     );
+  //////////////////////
+  const updateUserProfile = async (updatedProfileData) => {
+    try {
+      const response = await postRequest(
+        `${baseUrl}/auth/profile`,
+        JSON.stringify(updatedProfileData)
+      );
 
-  //     if (!response.error) {
-  //       setProfileData(updatedProfileData);
-  //       console.log("Profile updated successfully!");
-  //     } else {
-  //       console.error("Error updating profile:", response.message);
-  //     }
-  //   } catch (error) {
-  //     console.error("Error updating profile:", error);
-  //   }
-  // };
+      if (!response.error) {
+        setProfileData(updatedProfileData);
+        console.log("Profile updated successfully!");
+      } else {
+        console.error("Error updating profile:", response.message);
+      }
+    } catch (error) {
+      console.error("Error updating profile:", error);
+    }
+  };
 
   const handleEditClick = () => {
     setEditMode(true);
