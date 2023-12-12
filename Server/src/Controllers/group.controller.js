6a5a1c3afa9fd58387cfad8e758f53ab3e49c5f9
nntthuong <thuong.nn204855@gmail.com>
2023-12-12 21:45:36 +0700

    handle conflic

--- Server/src/Controllers/group.controller.js
+++ Server/src/Controllers/group.controller.js
@@ -1,158 +1,54 @@
-const authService = require("../services/auth.service");
-const bcrypt = require("bcrypt");
-const validator = require("validator");
-const jwt = require("jsonwebtoken");
-const userModel = require("../models/user.model");
-require("dotenv").config();
-
-const createToken = (_id) => {
-  const jwtkey = process.env.JWT_SECRET_KEY;
-  return jwt.sign({ _id }, jwtkey, { expiresIn: "3d" });
-};
-class authController {
-  async loginUser(req, res) {
+const groupModel = require("../models/group.model");
+const groupService = require("../services/group.service");
+class groupController {
+  async createGroup(req, res) {
     try {
-      const { email, password } = req.body;
-      console.log(req.body);
-      let user = await authService.getOneUser(email);
-      console.log(user);
-      if (!user)
-        return res
-          .status(400)
-          .json("Thông tin tài khoản hoặc mật khẩu không chính xác");
-
-      const isValidPassword = await bcrypt.compare(password, user.password);
-
-      if (!isValidPassword)
-        return res
-          .status(400)
-          .json("Thông tin tài khoản hoặc mật khẩu không chính xác");
-
-      const token = createToken(user._id);
-
-      user = { ...user.toObject(), password: undefined, token };
-
-      res.status(200).json(user);
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
-  }
-  async registerUser(req, res) {
-    try {
-      let password = req.body.password;
-      const email = req.body.email;
-      const name = req.body.name;
-      const numberPhone = req.body.numberPhone;
-      const gender = req.body.gender;
-      let avatar = "";
-      if (gender == "female") {
-        avatar =
-          "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-8.png";
+      const { members } = req.body;
+      if (members.length === 2) {
+        var group = await groupService.getGroup(members);
+        if (group) return res.status(201).json({ id: group });
       }
-      let user = await userModel.findOne({ email: email });
-      if (user) {
-        return res.status(400).json("Email đã được sử dụng");
+      const userCount = members.length;
+      let name;
+      if (userCount > 2) {
+        name = req.body.name;
       }
-
-      user = await userModel.findOne({ numberPhone: numberPhone });
-      if (user) {
-        return res.status(400).json("Số điện thoại đã được sử dụng");
+      let newGroup;
+      if (name) {
+        newGroup = new groupModel({
+          members: members,
+          userCount: userCount,
+          name: name,
+        });
+      } else {
+        newGroup = new groupModel({
+          members: members,
+          userCount: userCount,
+        });
       }
-
-      // Check for required fields
-      if (!name || !email || !password || !numberPhone) {
-        return res.status(400).json("Vui lòng điền các trường bắt buộc");
-      }
-
-      // Check for invalid email format
-      if (!validator.isEmail(email)) {
-        return res.status(400).json("Email không đúng định dạng");
-      }
-      if (!validator.isStrongPassword(password)) {
-        return res.status(400).json("Mật khẩu không đủ mạnh");
-      }
-
-      const salt = await bcrypt.genSalt(10);
-
-      password = await bcrypt.hash(password, salt);
-
-      req.body.password = password;
-
-      user = await authService.createUser(req.body);
-
-      const token = createToken(user._id);
-
-      user = { ...user.toObject(), password: undefined, token };
-
-      res.status(200).json(user);
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
-  }
-  async findUser(req, res) {
-    try {
-      const userId = req.params.userId;
-
-      var user = await authService.getUser(userId);
-      user = { ...user.toObject(), password: undefined };
-      res.status(200).json(user);
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
-  }
-  async getUser(req, res) {
-    try {
-      var users = await authService.getAllUsers();
-      const usersWithoutPasswords = users.map((user) => {
-        const { password, ...userWithoutPassword } = user.toObject();
-        return userWithoutPassword;
-      });
-      res.status(200).json(usersWithoutPasswords);
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
-  }
-
-  async getFriends(req, res) {
-    try {
-      let user_id = req.body.user_id;
-      var friends = await authService.getUser(user_id);
-
-      res.status(200).json(friends.friends);
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
-  }
-  async updateOnline(req, res) {
-    try {
-      let user_id = req.body.user_id;
-      var response = await authService.updateOnline(user_id);
-
+      const response = await groupService.createGroup(newGroup);
       res.status(200).json(response);
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
   }
-  async updateProfile(req, res) {
+  async findUserGroup(req, res) {
     try {
-      const user_id = req.body.user_id;
-      const name = req.body.name;
-      const numberPhone = req.body.numberPhone;
-      const address = req.body.address;
-      const avatar = req.body.avatar;
-      var response = await authService.updateProfile(
-        user_id,
-        name,
-        numberPhone,
-        address,
-        avatar
-      );
-
-      res.status(200).json(response);
+      const userID = req.params.userID;
+      const groups = await groupService.findUserGroup(userID);
+      return res.status(200).json(groups);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+  async findGroup(req, res) {
+    try {
+      const { firstID, secondID } = req.params;
+      const groups = await groupService.findGroup(firstID, secondID);
+      return res.status(200).json(groups);
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
   }
 }
-
-module.exports = new authController();
+module.exports = new groupController();
